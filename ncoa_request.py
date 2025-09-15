30f127cc53c8eb7dcb8edaa5bbe3689590669c8b
jjackson12 <jacob_jackson@brown.edu>
2025-09-15 15:25:46 -0600

    working update query to db

--- ncoa_request.py
+++ ncoa_request.py
@@ -3,23 +3,31 @@ from datetime import datetime
 import pandas as pd
 from google.cloud import bigquery
 from config import BIGQUERY_SERVICE_ACCOUNT_FILE
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def query_NCOA_data(input_data, test=False):
     if test:
         # NOTE: This assumes no rows in our input data that do not exist in the sample test output data
         sample_output = pd.read_csv("sample_ncoa_output.csv")
-        filtered_sample_output_data = input_data[
-            input_data["individual_id"].isin(sample_output["record_id"])
-        ]
+        merged_data = input_data.merge(
+            sample_output[["record_id", "record_type"]],
+            how="left",
+            left_on="individual_id",
+            right_on="record_id",
+        )
+        filtered_sample_output_data = merged_data.drop(columns=["record_id"])
         return filtered_sample_output_data
     else:
         # TODO: Implement actual NCOA query logic here
         return None
 
 
-# Initialize BigQuery client
-client = bigquery.Client.from_service_account_json(BIGQUERY_SERVICE_ACCOUNT_FILE)
+# Initialize BigQuery bq_client
+bq_client = bigquery.Client.from_service_account_json(BIGQUERY_SERVICE_ACCOUNT_FILE)
 
 # Generate a unique job_id and current datetime
 job_id = str(uuid.uuid4())
@@ -32,32 +40,32 @@ query_limit = None
 query = f"""
 SELECT
     vr_program_id AS individual_id,
-    first_name AS individual_first_name,
-    last_name AS individual_last_name,
+    first_name AS first_name,
+    last_name AS last_name,
     mail_addr1 AS address_line_1,
     mail_addr2 AS address_line_2,
-    mail_city AS address_city_name,
-    'NC' AS address_state_code,
-    mail_zipcode AS address_postal_code
+    mail_city AS address_city,
+    'NC' AS address_state,
+    mail_zipcode AS address_zipcode
 FROM `{source_table}`
 WHERE vr_program_id IS NOT NULL
 """
 if query_limit is not None:
     query += f"LIMIT {query_limit}"
-query_job = client.query(query)
+query_job = bq_client.query(query)
 results = query_job.result()
 
 # Convert query results to a DataFrame
 data = [
     {
         "individual_id": row.individual_id,
-        "individual_first_name": row.individual_first_name,
-        "individual_last_name": row.individual_last_name,
+        "first_name": row.first_name,
+        "last_name": row.last_name,
         "address_line_1": row.address_line_1,
         "address_line_2": row.address_line_2 or "",
-        "address_city_name": row.address_city_name,
-        "address_state_code": row.address_state_code,
-        "address_postal_code": row.address_postal_code,
+        "address_city": row.address_city,
+        "address_state": row.address_state,
+        "address_zipcode": row.address_zipcode,
     }
     for row in results
 ]
@@ -82,14 +90,14 @@ table_id = "vr-mail-generator.vr_data.ncoa_address_statuses"
 
 ncoa_response.rename(
     columns={
-        "record_id": "vr_program_id",
+        "individual_id": "vr_program_id",
         "first_name": "first_name",
         "last_name": "last_name",
         "address_line_1": "address_line_1",
         "city_name": "address_city",
         "state_code": "address_state",
         "postal_code": "address_zipcode",
-        "address_status": "ncoa_status",
+        "record_type": "ncoa_status",
     },
     inplace=True,
 )
@@ -100,7 +108,7 @@ job_config = bigquery.LoadJobConfig(
     write_disposition="WRITE_TRUNCATE",
     autodetect=True,  # <-- let BQ infer from df
 )
-client.load_table_from_dataframe(
+bq_client.load_table_from_dataframe(
     ncoa_response, temp_table_id, job_config=job_config
 ).result()
 
@@ -109,33 +117,33 @@ query = f"""
 MERGE `{table_id}` AS target
 USING `{temp_table_id}` AS source
 ON target.vr_program_id = source.vr_program_id
-   AND target.address_line_1 = source.address_line_1
-   AND target.address_city = source.address_city
-   AND target.address_zipcode = source.address_zipcode
-   AND target.address_state = source.address_state
+  AND target.address_line_1 = source.address_line_1
+  AND target.address_city = source.address_city
+  AND target.address_zipcode = source.address_zipcode
+  AND target.address_state = source.address_state
 WHEN MATCHED THEN
   UPDATE SET
-    last_attempted_update = CURRENT_TIMESTAMP(),
-    ncoa_status = CASE
-      WHEN target.ncoa_status != source.ncoa_status THEN source.ncoa_status
-      ELSE target.ncoa_status
-    END,
-    last_updated = CASE
-      WHEN target.ncoa_status != source.ncoa_status THEN CURRENT_TIMESTAMP()
-      ELSE target.last_updated
-    END
+   last_attempted_update = DATETIME(CURRENT_DATETIME()),
+   ncoa_status = CASE
+    WHEN target.ncoa_status != source.ncoa_status THEN source.ncoa_status
+    ELSE target.ncoa_status
+   END,
+   last_updated = CASE
+    WHEN target.ncoa_status != source.ncoa_status THEN DATETIME(CURRENT_DATETIME())
+    ELSE target.last_updated
+   END
 WHEN NOT MATCHED THEN
   INSERT (
-    vr_program_id, address_line_1, address_city, address_zipcode, address_state,
-    first_name, last_name, ncoa_status, last_updated, last_attempted_update
+   vr_program_id, address_line_1, address_city, address_zipcode, address_state,
+   first_name, last_name, ncoa_status, last_updated, last_attempted_update
   )
   VALUES (
-    source.vr_program_id, source.address_line_1, source.address_city, source.address_zipcode,
-    source.address_state, source.first_name, source.last_name, source.ncoa_status,
-    CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()
+   source.vr_program_id, source.address_line_1, source.address_city, source.address_zipcode,
+   source.address_state, source.first_name, source.last_name, source.ncoa_status,
+   DATETIME(CURRENT_DATETIME()), DATETIME(CURRENT_DATETIME())
   )
 """
-client.query(query).result()
+bq_client.query(query).result()
 
 # Clean up the temporary table
-client.delete_table(temp_table_id, not_found_ok=True)
+bq_client.delete_table(temp_table_id, not_found_ok=True)
